782471272930a65cf4498d860a6514e127fe724c
bhavya28122251 <bhavyarjoshi19@gmail.com>
2025-05-26 00:00:51 +0530

    HPA applied

--- Frontend/src/components/auth/Register.js
+++ Frontend/src/components/auth/Register.js
@@ -48,7 +48,7 @@ function Register() {
     setLoading(true);
 
     try {
-      await axios.post('http://patient-management.local/api/auth/register', {
+      await axios.post('http://localhost:8085/api/auth/register', {
         username: formData.username,
         fullName: formData.fullName,
         email: formData.email,
