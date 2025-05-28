56733b82547d3c56c86dd29ff710f145786abe9e
gitikapinjani <gitika.raipur123@gmail.com>
2025-05-28 11:42:28 +0530

    bug fixes

--- Backend/patient-management/auth-service/src/main/java/com/healthcare/auth/config/SecurityConfig.java
+++ Backend/patient-management/auth-service/src/main/java/com/healthcare/auth/config/SecurityConfig.java
@@ -39,8 +39,9 @@ public class SecurityConfig {
                 })
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
-                    config.addAllowedOrigin("http://localhost:3000","https://frontend.local",
-                               "https://healthcare-appointments.com"); // Add the origin of your front-end app
+                    config.addAllowedOrigin("http://localhost:3000");
+                    config.addAllowedOrigin("https://frontend.local");
+                    config.addAllowedOrigin("https://healthcare-appointments.com");
                     config.addAllowedMethod(HttpMethod.GET); // Allow GET method
                     config.addAllowedMethod(HttpMethod.POST); // Allow POST method
                     config.addAllowedMethod(HttpMethod.PUT); // Allow PUT method
